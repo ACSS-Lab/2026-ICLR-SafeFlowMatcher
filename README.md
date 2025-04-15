25fec918924286ae39b05806558224a28fe61628
Jeongyong Yang <engineer.seiryu@gmail.com>
2025-04-15 18:03:28 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# CBF-CFM
+
+
+# Installation
+```
+conda env create -f environment.yml
+conda activate safediffuser
+pip install -e .
+pip install qpth cvxpy cvxopt
+```
+
+# Switch between different experiments
+```
+git switch maze2d/locomotion/kuka
+```
+
+# Important notes for SafeDiffusers
+
+1. run the following code first before running any code to address potential bugs (mujoco200 is needed)
+```
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-515
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wei/.mujoco/mujoco200/bin
+```
+2. Choose diffusers/CG/Truncate/different safediffusers 
+
+(I) maze2d case:
+
+(1) Choose any one option betwee lines 1040 and 1095 of the file diffuser/models/diffusion.py
+
+(2) Choose any one option betwee lines 300 and 358 of the file diffuser/utils/rendering.py   (for visualization purpose)
+
+(3) Customize the diffusion output in the file diffuser/guides/policies.py  (__call__ function)
+
+(4) Customize the diffusion saving/print in the files scripts/plan_maze2d.py (especially after line 55) and config/maze2d.py
+
+(II) locomotion (walker2d and hopper) case:
+
+Choose any one option betwee lines 177 and 207 of the file diffuser/models/diffusion.py
+
+(III) Manipulation (kuka) case:
+
+Choose any one option betwee lines 405 and 439 of the file denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+
+
+# Use pre-trained models
+## 1. Downloading weights
+Download pretrained diffusion models and value functions (from diffuser in the locomotion branch) with:
+```
+./scripts/download_pretrained.sh
+```
+
+## 2. Planning using pre-trained models
+To plan with guided sampling, run:
+
+Maze2d:
+
+```
+python scripts/plan_maze2d.py --config config.maze2d --dataset maze2d-large-v1 --logbase logs/pretrained
+```
+
+Locomotion:
+
+```
+python scripts/plan_guided.py --dataset walker2d-medium-expert-v2 --logbase logs/pretrained
+```
+
+Manipulation (unconditional):
+
+```
+python scripts/unconditional_kuka_planning_eval.py
+```
+
+The --logbase points the experiment loader to the folder containing the pretrained/self-trained models.
+
+# Training from scratch
+## 1. Train a diffusion model with:
+
+Maze2d:
+
+```
+python scripts/train.py --config config.maze2d --dataset maze2d-large-v1
+```
+
+Locomotion:
+
+```
+python scripts/train.py --dataset walker2d-medium-expert-v2
+```
+
+Manipulation:
+
+```
+python scripts/kuka.py
+```
+
+The default hyperparameters are listed in maze2d/locomotion:diffusion. You can override any of them with flags, eg, --n_diffusion_steps 100.
+
+## 2. Train a value function with:
+```
+python scripts/train_values.py --dataset walker2d-medium-expert-v2
+```
+See locomotion:values for the corresponding default hyperparameters.
+
+## 3. Plan using your newly-trained models with the same command as in the pretrained planning section, simply replacing the logbase to point to your new models:
+
+Maze2d:
+
+```
+python scripts/plan_maze2d.py --config config.maze2d --dataset maze2d-large-v1 --logbase logs
+```
+
+Locomotion:
+
+```
+python scripts/plan_guided.py --dataset walker2d-medium-expert-v2 --logbase logs
+```
+
+Manipulation(unconditional): 
+
+```
+python scripts/unconditional_kuka_planning_eval.py
+```
+
+# Reference
+TBD
+
+# Acknowledgements
+The diffusion model implementation and organization are based on Michael Janner's diffuser repo: https://github.com/jannerm/diffuser
+The Safe diffuser implementation and organization are bsed on Xiao's SafeDiffuser repo: https://github.com/Weixy21/SafeDiffuser
---
+++ diffuser/models/cfm.py
@@ -0,0 +1,195 @@
+import numpy as np
+import torch
+from torch import nn
+import pdb
+import torchdiffeq
+
+import diffuser.utils as utils
+from .helpers import (
+    cosine_beta_schedule,
+    extract,
+    apply_conditioning,
+    Losses,
+)
+
+from torchcfm.conditional_flow_matching import ConditionalFlowMatcher
+from torchdyn.core import NeuralODE
+
+class CFM(nn.Module):
+    def __init__(self, model, horizon, observation_dim, action_dim, n_timesteps=1000,
+        loss_type='l1', clip_denoised=False, predict_epsilon=True,
+        action_weight=1.0, loss_discount=1.0, loss_weights=None,
+    ):
+        super().__init__()
+        self.horizon = horizon
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.transition_dim = observation_dim + action_dim
+        self.model = model
+
+        # CFM setting
+        sigma = 0.0
+        self.FM = ConditionalFlowMatcher(sigma=sigma)
+        self.node = NeuralODE(model, solver="dopri5", sensitivity="adjoint", atol=1e-4, rtol=1e-4)
+
+        # Settings for compatibility with diffusion models
+        betas = cosine_beta_schedule(n_timesteps)
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+
+        self.n_timesteps = int(n_timesteps)
+        self.clip_denoised = clip_denoised
+        self.predict_epsilon = predict_epsilon
+
+        self.register_buffer('betas', betas)
+        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        self.register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1. - alphas_cumprod))
+        self.register_buffer('log_one_minus_alphas_cumprod', torch.log(1. - alphas_cumprod))
+        self.register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1. / alphas_cumprod))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_cumprod - 1))
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+        self.register_buffer('posterior_variance', posterior_variance)
+
+        ## log calculation clipped because the posterior variance
+        ## is 0 at the beginning of the diffusion chain
+        self.register_buffer('posterior_log_variance_clipped',
+            torch.log(torch.clamp(posterior_variance, min=1e-20)))
+        self.register_buffer('posterior_mean_coef1',
+            betas * np.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
+        self.register_buffer('posterior_mean_coef2',
+            (1. - alphas_cumprod_prev) * np.sqrt(alphas) / (1. - alphas_cumprod))
+
+        ## get loss coefficients and initialize objective
+        loss_weights = self.get_loss_weights(action_weight, loss_discount, loss_weights)
+        self.loss_fn = Losses[loss_type](loss_weights, self.action_dim)
+
+    def get_loss_weights(self, action_weight, discount, weights_dict):
+        '''
+            sets loss coefficients for trajectory
+
+            action_weight   : float
+                coefficient on first action loss
+            discount   : float
+                multiplies t^th timestep of trajectory loss by discount**t
+            weights_dict    : dict
+                { i: c } multiplies dimension i of observation loss by c
+        '''
+        self.action_weight = action_weight
+
+        dim_weights = torch.ones(self.transition_dim, dtype=torch.float32)
+
+        ## set loss coefficients for dimensions of observation
+        if weights_dict is None: weights_dict = {}
+        for ind, w in weights_dict.items():
+            dim_weights[self.action_dim + ind] *= w
+
+        ## decay loss with trajectory timestep: discount**t
+        discounts = discount ** torch.arange(self.horizon, dtype=torch.float)
+        discounts = discounts / discounts.mean()
+        loss_weights = torch.einsum('h,t->ht', discounts, dim_weights)
+
+        ## manually set a0 weight
+        loss_weights[0, :self.action_dim] = action_weight
+        return loss_weights
+
+    #------------------------------------------ sampling ------------------------------------------#
+
+    @torch.no_grad()
+    def conditioned_ode_func(self, t, x, cond):
+        """
+        Computes the ODE vector field with conditioning at each step
+        """
+        # 1. Apply condition to current state
+        x_cond = apply_conditioning(x, cond, self.action_dim)
+        
+        # 2. Compute vector field on the conditioned state
+        t_batch = torch.full((x.shape[0],), t, device=x.device)
+        vt = self.model(x_cond, None, t_batch)
+        
+        return vt
+
+    @torch.no_grad()
+    def p_sample_loop(self, shape, cond, verbose=True, return_diffusion=False):
+        """
+        Generate samples by solving the conditional ODE
+        """
+        # Initial noise
+        x0 = torch.randn(shape).to(self.device)
+        
+        # Apply condition to initial state
+        x0 = apply_conditioning(x0, cond, self.action_dim)
+        
+        # Wrapper function for odeint (only accepts t and x)
+        ode_fn = lambda t, x: self.conditioned_ode_func(t, x, cond)
+        
+        # Solve ODE using wrapper
+        traj = torchdiffeq.odeint(
+            ode_fn,
+            x0,
+            torch.linspace(0, 1, self.n_timesteps + 1).to(self.device),
+            atol=1e-4,
+            rtol=1e-4,
+            method="euler",
+        )
+        
+        x1 = traj[-1]
+        # Apply condition again at the end (for safety)
+        x1 = apply_conditioning(x1, cond, self.action_dim)
+        
+        if return_diffusion:
+            return traj
+        return x1
+
+    @torch.no_grad()
+    def conditional_sample(self, cond, *args, horizon=None, **kwargs):
+        '''
+            conditions : [ (time, state), ... ]
+        '''
+        device = self.betas.device
+        batch_size = len(cond[0])
+        horizon = horizon or self.horizon
+        shape = (batch_size, horizon, self.transition_dim)
+
+        return self.p_sample_loop(shape, cond, *args, **kwargs)
+
+    #------------------------------------------ training ------------------------------------------#
+    
+    @property
+    def device(self):
+        """Get the device where the model's parameters are allocated."""
+        # Assumes the model's parameters are all on the same device.
+        return next(self.parameters()).device
+
+    def loss(self, x, cond):
+        x = x.to(self.device)
+        batch_size = len(x)
+
+        t = torch.rand(batch_size, device=x.device)
+        
+        x1 = x.to(self.device)
+        x0 = torch.randn_like(x1)
+
+        # Generate xt and flow field ut at time t
+        t, xt, ut = self.FM.sample_location_and_conditional_flow(x0, x1)
+
+        # Apply condition
+        xt = apply_conditioning(xt, cond, self.action_dim)
+
+        # Compute vector field
+        vt = self.model(xt, None, t) # if there are cond, modify None -> cond
+
+        # Compute loss
+        loss, info = self.loss_fn(vt, ut)
+        
+        return loss, info
+    
+
+    def forward(self, cond, *args, **kwargs):
+        return self.conditional_sample(cond=cond, *args, **kwargs)
